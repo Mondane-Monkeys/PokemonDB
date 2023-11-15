58d9361a376e5e393a58747b8915f0666cc57ade
Mondane-Monkeys <65215510+Mondane-Monkeys@users.noreply.github.com>
2023-11-15 13:03:16 -0600

    Setup UI

---
+++ Scripts/UI/db/pokemon.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS pokemon(
+   id INTEGER PRIMARY KEY,
+   name TEXT,
+);
+
+
+INSERT INTO pokemon values (1, "Bulbasaur");
+INSERT INTO pokemon values (2, "Ivysaur");
+INSERT INTO pokemon values (3, "Venusaur");
---
+++ Scripts/UI/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sql.DB
+var queryMetadata map[string]QueryMetadata
+
+const DbPath = "./db/pokemon.db"
+const SqlPath = "./db/pokemon.db"
+const QueriesPath = "./db/queries"
+const QueriesMetaPath = "./db/queries.json"
+
+const port = ":8765"
+
+func main() {
+	// Open SQLite3 database
+	var err error
+	db, err = sql.Open("sqlite3", DbPath)
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		return
+	}
+	// "defer" closes the connection automatically when main() terminates
+	defer db.Close()
+
+	// Create tables if not exists
+	createTables()
+
+	// Load query metadata from queries.json
+	queryMetadata, err := loadQueryMetadata(QueriesMetaPath)
+	if err != nil {
+		fmt.Println("Error loading query metadata:", err)
+		return
+	}
+	fmt.Println("Query metadata", queryMetadata)
+
+	// API endpoint to execute queries
+	http.HandleFunc("/api/execute_query", executeQueryHandler)
+
+	// API endpoint to get query metadata
+	http.HandleFunc("/api/get_query_list", getQueryListHandler)
+
+	http.HandleFunc("/api/test", testEndpoint)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	fmt.Println("Server listening on ", port, "...")
+	http.ListenAndServe(port, nil)
+}
+
+func createTables() {
+	// Read the SQL initialization script from pokemon.sql
+	sqlScript, err := os.ReadFile("testdata/hello")
+	if err != nil {
+		fmt.Println("Error reading pokemon.sql:", err)
+		return
+	}
+
+	// Execute the SQL initialization script
+	_, err = db.Exec(string(sqlScript))
+	if err != nil {
+		fmt.Println("Error executing SQL script:", err)
+	}
+}
+
+func loadQueryMetadata(filename string) (map[string]QueryMetadata, error) {
+	// Load query metadata from queries.json
+	fileContent, err := os.ReadFile("testdata/hello")
+	if err != nil {
+		return nil, err
+	}
+
+	var queryMetadata map[string]QueryMetadata
+	if err := json.Unmarshal(fileContent, &queryMetadata); err != nil {
+		return nil, err
+	}
+
+	return queryMetadata, nil
+}
+
+// QueryMetadata represents the metadata for a query
+type QueryMetadata struct {
+	Parameters []interface{} `json:"parameters"`
+	SQLFile    string        `json:"sql_file"`
+}
+
+////////////////////////////////////
+///////////// ENDPOINT /////////////
+////////////////////////////////////
+
+func executeQueryHandler(w http.ResponseWriter, r *http.Request) {
+	var queryRequest QueryRequest
+	if err := json.NewDecoder(r.Body).Decode(&queryRequest); err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	// Check if the query name exists in the metadata
+	metadata, ok := queryMetadata[queryRequest.QueryName]
+	if !ok {
+		http.Error(w, "Query not found", http.StatusNotFound)
+		return
+	}
+
+	// Validate the parameters
+	if len(queryRequest.Parameters) != len(metadata.Parameters) {
+		http.Error(w, "Invalid number of parameters", http.StatusBadRequest)
+		return
+	}
+
+	// Execute the query (implementation omitted for simplicity)
+	result, err := executeQuery(metadata.SQLFile, queryRequest.Parameters)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Return the result
+	json.NewEncoder(w).Encode(result)
+}
+
+func getQueryListHandler(w http.ResponseWriter, r *http.Request) {
+	// Return the query metadata
+	fmt.Println("Endpoint hit: GET /api/get_query_list")
+	json.NewEncoder(w).Encode(queryMetadata)
+	fmt.Fprintf(w, "hello\n")
+}
+
+func executeQuery(sqlFile string, parameters []interface{}) (interface{}, error) {
+	fmt.Println("Endpoint hit: POST /api/execute_query")
+	// Implementation to execute the query (e.g., read SQL from file, bind parameters, execute)
+	// ...
+
+	// For simplicity, return a placeholder result
+	return "Query executed successfully", nil
+}
+
+func testEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: test")
+	fmt.Fprintf(w, "{\"name\":\"Bulbasaur\", \"id\":\"1\"}")
+}
+
+// QueryRequest represents the request format for executing a query
+type QueryRequest struct {
+	QueryName  string        `json:"query_name"`
+	Parameters []interface{} `json:"parameters"`
+}
